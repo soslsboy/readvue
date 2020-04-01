c184b4b1618bc5d18046a80bf176ca60c183e4d5
liuqi <604960030@qq.com>
2020-04-01 19:16:40 +0800

    更新vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     outputDir: 'dist',
-    publicPath: process.env.NODE_ENV === 'production' ? '/[项目名]/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/readvue/' : '/'
 }
